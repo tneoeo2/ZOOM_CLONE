3fa17fac96df3ab763953aca18d98c4c695a0641
tneoeo2 <tneoeo2@gmail.com>
2022-06-26 01:16:04 +0900

    220626 websocket001

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,31 @@
-alert("hi");
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('form');
+//socket : 서버로의 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function handleOpen(){
+    console.log("Connected to Server!");
+}
+
+function handleClose(){
+    console.log("Discconnected from Server! ")
+}
+
+socket.addEventListener("message", (message)=>{
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
+
+socket.addEventListener("close", handleClose);
+
+
+function handleSubmit(event){
+    event.preventDefault();   //키입력이 input칸 채우는 것을 방지
+    const input = messageForm.querySelector('input');
+    socket.send(input.value);
+    input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
+
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, {WebSocketServer} from "ws";
 import express from "express";   //express view 설정, render해주는 역할
 
 const app = express();
@@ -7,8 +9,32 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));   //frontend에서 구동되는 코드(app.js) (user가 볼수 있는 폴더)
 app.get("/", (req, res) => res.render("home"));   //home에서 res, req를 받고 home을 렌더해줌 (template 렌더)
 app.get("/*", (req, res) => res.redirect("/"));  //home 외의 나머지 주소 redirect
-const handleListen = () => console.log('Lintening on http://localhost:3000');
-app.listen(3000, handleListen); 
+const handleListen = () => console.log('Lintening on http://localhost:3000');   //? ws://localhost:3000  => ws / http 요청 둘 다 처리 가능
+// app.listen(3000, handleListen); 
+
+const server = http.createServer(app);      //express app로부터 서버 만들기   //? httpServer
+const wss = new WebSocketServer({ server }); //?websocketServer    ==> websocker 서버 && httpServer 둘다 돌릴 수 있다!!
+
+
+function onSocketClose(){
+    console.log("Disconnected from the Browser X");
+}
+
+const sockets = [];
+
+//socket : 브라우저로의 연결
+wss.on("connection", (socket) =>{
+    sockets.push(socket);
+    console.log("Connected to Browser!");
+    socket.on("close", onSocketClose); //서버 닫혔을때 실행
+    socket.on("message", (message)=>{
+        sockets.forEach(aSocket => aSocket.send(message.toString("utf-8")));    //sockets안의 브라우저에게 모두 메시지 전송
+    });      //브라우저에서 보내기
+});
+
+server.listen(3000, handleListen);
+
+
 
 //nodemon  ignore 설정으로 server 수정시에만 서버 새로고침되게 변경
 //server.js    node(backend에서 구동되는 코드)
