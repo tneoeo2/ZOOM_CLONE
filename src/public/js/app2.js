f2cd5a00f7f631937ffd0877fad428d81b8e0806
tneoeo2 <tneoeo2@gmail.com>
2022-07-03 02:15:07 +0900

    220703 WebRTC  video 띄우기

---
+++ src/public/js/app2.js
@@ -0,0 +1,49 @@
+const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;     //디폴트 값 설정
+let cameraOff = false;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    console.log(myStream);
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
+getMedia();
+
+function handleMuteClick() {
+  if(!muted){
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  }else{
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  if(cameraOff){
+    cameraBtn.innerText = "Turn Camera Off"
+    cameraOff = false;
+  }else{
+    cameraBtn.innerText = "Turn Camera On"
+    cameraOff = true;
+  }
+
+}
+
+muteBtn.addEventListener("click", handleMuteClick); 
+cameraBtn.addEventListener("click", handleCameraClick);
---
+++ src/server2.js
@@ -0,0 +1,17 @@
+import http from "http";
+import SocketIO from "socket.io";
+import express from "express";
+const app = express();
+
+app.set("view engine", "pug");
+
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home2"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+const handleListen = () => console.log('Lisenting on http://localhost:3000');
+httpServer.listen(3000, handleListen);
